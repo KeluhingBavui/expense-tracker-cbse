065d19e99c32dc5e33dcd1449a4365e4d7508c8d
alif <alifmazli01@gmail.com>
2023-12-12 13:51:23 +0800

    Add userId

--- src/main/java/com/cbse/expensetracker/shared/entity/Loan.java
+++ src/main/java/com/cbse/expensetracker/shared/entity/Loan.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.annotation.Id;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Table;
@@ -30,12 +32,16 @@ public class Loan {
   private String status;
   private String reason;
 
-  public Loan(LocalDate date, float amount, String type, String person, String status, String reason) {
+  @Column(name = "user_id")
+  private UUID userId;
+
+  public Loan(LocalDate date, float amount, String type, String person, String status, String reason, UUID userId) {
     this.date = date;
     this.amount = amount;
     this.type = type;
     this.person = person;
     this.status = status;
     this.reason = reason;
+    this.userId = userId;
   }
 }
