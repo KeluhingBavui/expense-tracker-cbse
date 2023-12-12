947c0897aeb7c80456e655f4f87914bc65f5fd17
alif <alifmazli01@gmail.com>
2023-12-12 13:51:44 +0800

    Init LoansService interface

---
+++ src/main/java/com/cbse/expensetracker/loans/LoansService.java
@@ -0,0 +1,17 @@
+package com.cbse.expensetracker.loans;
+
+import com.cbse.expensetracker.shared.entity.Loan;
+import java.util.List;
+import java.util.UUID;
+
+public interface LoansService {
+  List<Loan> getLoansByUserId(UUID userId);
+
+  Loan saveLoan(Loan loan);
+
+  void deleteLoanById(UUID loanId);
+
+  float calculateToRepayByUserId(UUID userId);
+
+  float calculateToReceiveByUserId(UUID userId);
+}
