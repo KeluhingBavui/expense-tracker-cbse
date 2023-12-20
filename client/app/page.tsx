2690b3f5e5d4685f1f0867be3ef650c8e8c58e1b
KeluhingBavui <daniel01.jonathan@gmail.com>
2023-12-21 03:35:10 +0800

    Refactor page component and add expense fetching logic

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,57 @@
-
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import DisplayCard from "@/components/display-card";
+import ExpenseTable from "@/components/expense-table";
+import { Expense } from "@/types/expense";
+import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
+
+async function getExpenses(userId?: string, categoryId?: string): Promise<Expense[] | undefined> {
+  try {
+    let response: Response;
+
+    if (userId) {
+      response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/v1/expenses?userId=${userId}`,{
+          method: "GET",
+        }
+      );
+    } else if (categoryId) {
+      response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/v1/expenses?categoryId=${categoryId}`,{
+          method: "GET",
+        }
+      );
+    } else {
+      throw new Error("No userId or categoryId provided");
+    }
+  
+    if (!response.ok) {
+      throw new Error("Error fetching expenses: " + response.statusText + " " + response.json());
+    }
+  
+    const expenses: Expense[] = await response.json();
+
+    return expenses;
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+}
 
 export default async function Home() {
-  const supabase = createServerComponentClient({ cookies });
+  const cookieStore = cookies();
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+      },
+    }
+  )
 
   const {
     data: { session },
@@ -14,9 +61,25 @@ export default async function Home() {
     redirect("/login");
   }
 
+  const expenses = await getExpenses(session.user.id);
+
+  if (!expenses) {
+    throw new Error("Error fetching expenses");
+  }
+
   return (
-    <>
-      <h1>Expenses Tracker Application</h1>
-    </>
+    <div className="grid gap-4">
+      <p className="text-4xl">Expenses</p>
+
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+        <DisplayCard title="Food" content="$100" />
+        <DisplayCard title="Food" content="$100" />
+        <DisplayCard title="Food" content="$100" />
+        <DisplayCard title="Food" content="$100" />
+      </div>
+
+      {/* Expense Table */}
+      <ExpenseTable expenses={expenses} />
+    </div>
   );
 }
