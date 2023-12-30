9438492f4850dadb2b732eee5dd019bdc01b1cf6
Omar Al Hendi <omar.hendi@rihal.om>
2023-12-31 03:16:57 +0800

    feat(web): add the settings page UI

--- client/app/settings/page.tsx
+++ client/app/settings/page.tsx
@@ -1,26 +1,48 @@
 import React from "react";
 import { axios } from "@/lib/axios";
-import Currency from "@/components/settings/Currency";
-import CurrenciesList from "@/components/settings/CurrenciesList";
+import { Field as CurrencyField, List } from "@/components/settings/Currency";
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+import { Separator } from "@/components/ui/separator";
+import Field from "@/components/settings/Field";
 
-const fetchSettings = async () => {
+const fetchSettings = async (userId: string) => {
   const { data } = await axios.get(`/settings`, {
-    params: {
-      // todo: send the user id to all requests (meybe in a middleware)
-      userId: "d94a860f-b66f-46e2-aa29-4c918dd5c848",
-    },
+    params: { userId },
   });
   return data;
 };
 
 const SettingsPage = async () => {
-  const settings = await fetchSettings();
-  console.log(settings);
+  const cookieStore = cookies();
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+      },
+    }
+  );
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const settings = await fetchSettings(session?.user.id!);
   return (
     <div>
-      <h1> Settings Page</h1>
-      <Currency currency={settings.currency} />
-      <CurrenciesList />
+      <h1 className="font-bold text-4xl mb-2"> Settings</h1>
+      <Separator className="h-[3px] bg-white mb-4" />
+      <div className="grid grid-cols-2 items-start">
+        <div className="grid gap-8">
+          <Field label="Language" value={settings.language} />
+          <Field label="Font" value={settings.font} />
+          <Field label="Theme" value={settings.theme} />
+          <CurrencyField currency={settings.currency} />
+        </div>
+        <List />
+      </div>
     </div>
   );
 };
