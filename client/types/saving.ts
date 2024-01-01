8e4ccfd239edea3080473104592fba8a57c4f9a9
Syaamil Faiq <syaamil1136@gmail.com>
2024-01-02 01:09:30 +0800

    Add 'to_save_amount' field to Saving type

--- client/types/saving.ts
+++ client/types/saving.ts
@@ -3,6 +3,7 @@ export type Saving = {
     target_date: string;
     target_amount: number;
     saving_amount: number;
+    to_save_amount: number;
     purpose: string;
     comment: string;
     userId: string;
