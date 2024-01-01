66b5fe32595e16290ae768b9e003e22bc6c9d920
Syaamil Faiq <syaamil1136@gmail.com>
2024-01-02 00:31:23 +0800

    Add Saving type definition

---
+++ client/types/saving.ts
@@ -0,0 +1,9 @@
+export type Saving = {
+    id: string;
+    target_date: string;
+    target_amount: number;
+    saving_amount: number;
+    purpose: string;
+    comment: string;
+    userId: string;
+}
