68c6ca4b1178d54277b6dc481f0f3bd5a623563c
kyuuurina <17204442@siswa.um.edu.my>
2024-01-13 20:02:16 +0800

    notif types ui

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -3,14 +3,22 @@
 import { useRouter } from 'next/navigation';
 import {
   NavigationMenu,
+  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from '@/components/ui/navigation-menu';
 import { createBrowserClient } from '@supabase/ssr';
+import { Notification } from '@/types/notification';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Separator } from '@/components/ui/separator';
+interface NavbarProps {
+  notifications: Notification[] | undefined;
+}
 
-export default function Navbar() {
+export default function Navbar({ notifications }: NavbarProps) {
   const supabase = createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
@@ -22,6 +30,8 @@ export default function Navbar() {
     await supabase.auth.signOut();
     router.refresh();
   }
+
+  console.log(notifications);
 
   return (
     <>
@@ -66,6 +76,38 @@ export default function Navbar() {
                 Logout
               </NavigationMenuLink>
             </button>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Notifications</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ScrollArea className="h-72 w-64 rounded-md border">
+                <div className="p-4">
+                  <h4 className="mb-4 text-sm font-medium leading-none">
+                    Notifications
+                  </h4>
+                  {notifications?.length ? (
+                    notifications.map(
+                      (notification) =>
+                        notification.webEnabled && (
+                          <div
+                            key={notification.id}
+                            className="flex h-full w-full select-none flex-col justify-end rounded-md p-2 no-underline outline-none focus:shadow-md"
+                          >
+                            <div className="mb-2 mt-4 text-sm font-normal">
+                              {/* {notification.message} */}
+                              {notification.message}
+                            </div>
+                          </div>
+                        )
+                    )
+                  ) : (
+                    <li>
+                      <p>No notifications</p>
+                    </li>
+                  )}
+                </div>
+              </ScrollArea>
+            </NavigationMenuContent>
           </NavigationMenuItem>
         </NavigationMenuList>
       </NavigationMenu>
---
+++ client/types/notification.ts
@@ -0,0 +1,8 @@
+export type Notification = {
+  id: string;
+  message: string;
+  type: string;
+  user_id: string;
+  webEnabled: boolean;
+  createdAt: string;
+};
